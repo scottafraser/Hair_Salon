cd5774446bf60d67f2763fa40eb72d24dbf1df1b
Scott Fraser <scottafraser@yahoo.com>
2018-07-13 10:04:12 -0700

    add getClient method and test

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -111,6 +111,7 @@ namespace HairSalon.Models
             }
             return newStylist;
         }
+
         public static void DeleteAll()
         {
             MySqlConnection conn = DB.Connection();
@@ -124,5 +125,37 @@ namespace HairSalon.Models
                 conn.Dispose();
             }
         }
+
+        public List<Client> GetClients()
+        {
+            List<Client> allStylistClients = new List<Client> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = @stylist_id;";
+
+            MySqlParameter stylistId = new MySqlParameter();
+            stylistId.ParameterName = "@stylist_id";
+            stylistId.Value = this._id;
+            cmd.Parameters.Add(stylistId);
+
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int clientId = rdr.GetInt32(0);
+                string clientDescription = rdr.GetString(1);
+                int clientStylistId = rdr.GetInt32(2);
+                Client newClient = new Client(clientDescription, clientStylistId, clientId);
+                allStylistClients.Add(newClient);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allStylistClients;
+        }
+
     }
 }
