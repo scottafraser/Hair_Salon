ab001acd954d4528b41890682274ca39b243764d
Scott Fraser <scottafraser@yahoo.com>
2018-07-13 08:21:31 -0700

    Initial commit, Projeoject setup

---
+++ HairSalon.Tests/ControllerTests/ControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HairSalon.Tests
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
